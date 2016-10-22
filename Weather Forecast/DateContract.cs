9ef9d7633a64294852d0b67dece4e7dbb7aafb7a
Alijy <karamiali19@gmail.com>
2016-10-22 23:36:23 +0100

    A console application for fetching weather forecast for UK cities.

---
+++ Weather Forecast/DateContract.cs
@@ -0,0 +1,175 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace WeatherForecast
+{
+    [DataContract]
+    public class Response
+    {
+        /*coord:
+         *      lon.
+         *      lat.
+         *weather:
+         *      id.
+         *      main.
+         *      description.
+         *      icon.
+         *base.
+         *main:
+         *      temp.
+         *      pressure.
+         *      humidity.
+         *      temp_min.
+         *      temp_max.
+         *      sea_level.
+         *      grnd_level.
+         *wind:
+         *      speed.
+         *      deg.
+         *clouds:
+         *      all.
+         *rain:
+         *      3h.
+         *snow:
+         *      3h.
+         *dt.
+         *sys:
+         *      type.
+         *      id.
+         *      message.
+         *      country.
+         *      sunrise.
+         *      sunset.
+         *id.
+         *name.            
+         *cod.
+         */ 
+        [DataMember(Name = "coord")]
+        public Coord Coord { get; set; }
+
+        [DataMember(Name = "weather")]
+        public Weather[] Weather { get; set; }
+
+        [DataMember(Name = "base")]
+        public string Base { get; set; }
+
+        [DataMember(Name = "main")]
+        public Main Main { get; set; }
+
+        [DataMember(Name = "wind")]
+        public Wind Wind { get; set; }
+
+        [DataMember(Name = "clouds")]
+        public Clouds Clouds { get; set; }
+
+        [DataMember(Name = "rain")]
+        public Rain Rain { get; set; }
+
+        [DataMember(Name = "snow")]
+        public Snow Snow { get; set; }
+
+        [DataMember(Name = "dt")]
+        public long Dt { get; set; }
+
+        [DataMember(Name = "sys")]
+        public Sys Sys { get; set; }
+
+        [DataMember(Name = "id")]
+        public long ID { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "cod")]
+        public long Cod { get; set; }
+    }
+
+
+    [DataContract]
+    public class Coord
+    {
+        [DataMember(Name = "lon")]
+        public float Lon { get; set; }
+        [DataMember(Name = "lat")]
+        public float Lat { get; set; }
+    }
+
+    [DataContract]
+    public class Weather
+    {
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+        [DataMember(Name = "main")]
+        public string Main { get; set; }
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember(Name = "temp")]
+        public float Temp { get; set; }
+        [DataMember(Name = "pressure")]
+        public float Pressure { get; set; }
+        [DataMember(Name = "humidity")]
+        public int Humidity { get; set; }
+        [DataMember(Name = "temp_min")]
+        public float Temp_Min { get; set; }
+        [DataMember(Name = "temp_max")]
+        public float Temp_Max { get; set; }
+        [DataMember(Name = "sea_level")]
+        public float Sea_Level { get; set; }
+        [DataMember(Name = "grnd_level")]
+        public float Grnd_Level { get; set; }
+    }
+
+    [DataContract]
+    public class Wind
+    {
+        [DataMember(Name = "speed")]
+        public float Speed { get; set; }
+        [DataMember(Name = "deg")]
+        public float Deg { get; set; }
+    }
+
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember(Name = "all")]
+        public int All { get; set; }
+    }
+
+    [DataContract]
+    public class Rain
+    {
+        [DataMember(Name = "3h")]
+        public float R3h { get; set; }
+    }
+
+    [DataContract]
+    public class Snow
+    {
+        [DataMember(Name = "3h")]
+        public float S3h { get; set; }
+    }
+
+    [DataContract]
+    public class Sys
+    {
+        [DataMember(Name = "type")]
+        public float Type { get; set; }
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "message")]
+        public float Message { get; set; }
+        [DataMember(Name = "country")]
+        public string Country { get; set; }
+        [DataMember(Name = "sunrise")]
+        public long Sunrise { get; set; }
+        [DataMember(Name = "sunset")]
+        public long Sunset { get; set; }
+    }
+}
--- Weather Forecast/Program.cs
+++ Weather Forecast/Program.cs
@@ -1,15 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
 
-namespace Weather_Forecast
+namespace WeatherForecast
+
 {
     class Program
     {
+        static string openWeatherKey = "49358f41afe98dcd54f0a7cca0fa0717";
         static void Main(string[] args)
         {
+            Console.Write("Enter UK City Name:  ");
+            string city = Console.ReadLine();
+            try
+            {
+                string weatherRequest = CreateRequest(city);
+                Response weatherResponse = MakeRequest(weatherRequest);
+                ProcessResponse(weatherResponse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+
+        }
+
+        //Create the request URL
+        public static string CreateRequest(string queryString)
+        {
+            string UrlRequest = "http://api.openweathermap.org/data/2.5/weather?q=" +
+                                queryString + ",GB&units=metric&&APPID=" + openWeatherKey;
+            return (UrlRequest);
+        }
+
+        public static Response MakeRequest(string requestUrl)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format(
+                        "Server error (HTTP {0}: {1}).",
+                        response.StatusCode,
+                        response.StatusDescription));
+                    DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
+                    object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
+                    Response jsonResponse
+                    = objResponse as Response;
+                    return jsonResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+        static public void ProcessResponse(Response weatherResponse)
+        {
+
+            Console.WriteLine("\n");
+            Console.WriteLine(weatherResponse.Name + ", " + weatherResponse.Sys.Country);
+            Console.WriteLine("========================");
+            Console.WriteLine("Temperature : " + Math.Round(weatherResponse.Main.Temp) + " °C");
+            Console.WriteLine("Wind Speed : " + weatherResponse.Wind.Speed + " m/s");
+            Console.WriteLine("Humidity : " + weatherResponse.Main.Humidity + "%");
+            Console.WriteLine("Cloudiness : " + weatherResponse.Clouds.All + "%");
+            Console.WriteLine("Sky : " + weatherResponse.Weather[0].Description);
+            Console.WriteLine("\n");
+            Console.WriteLine(UnixTimeStampToDateTime(weatherResponse.Dt));
+            Console.WriteLine("========================");
+            Console.WriteLine("\n\n\n");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
